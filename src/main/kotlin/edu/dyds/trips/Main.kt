abfdc34955b8c3fb121302b2cad6512e3c36a659
Santiago Herrero <112264710+sanherrero@users.noreply.github.com>
2026-06-18 10:29:30 -0300

    splash screen
    pantalla de bienvenida

--- src/main/kotlin/edu/dyds/trips/Main.kt
+++ src/main/kotlin/edu/dyds/trips/Main.kt
@@ -1,17 +1,27 @@
 package edu.dyds.trips
 
 import edu.dyds.trips.di.TripsDependencyInjector
+import edu.dyds.trips.presentation.showSplashScreen
 import edu.dyds.trips.presentation.navigation.createAndShowAppWindow
+import kotlin.concurrent.thread
 import javax.swing.SwingUtilities
 
 fun main() {
     SwingUtilities.invokeLater {
-        createAndShowAppWindow(
-            scope = TripsDependencyInjector.appScope(),
-            homeViewModel = TripsDependencyInjector.createHomeViewModel(),
-            tripsViewModel = TripsDependencyInjector.createTripsViewModel(),
-            createDetailViewModel = { TripsDependencyInjector.createDetailViewModel() },
-            onClose = { TripsDependencyInjector.shutdown() }
-        )
+        val splash = showSplashScreen()
+
+        thread(name = "splash-delay") {
+            Thread.sleep(1500)
+            SwingUtilities.invokeLater {
+                createAndShowAppWindow(
+                    scope = TripsDependencyInjector.appScope(),
+                    homeViewModel = TripsDependencyInjector.createHomeViewModel(),
+                    tripsViewModel = TripsDependencyInjector.createTripsViewModel(),
+                    createDetailViewModel = { TripsDependencyInjector.createDetailViewModel() },
+                    onClose = { TripsDependencyInjector.shutdown() }
+                )
+                splash?.dispose()
+            }
+        }
     }
 }
